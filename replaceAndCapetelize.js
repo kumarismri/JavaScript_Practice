bc1af8beb696c7188fce0736f20ba3d040c73087
Smriti Kumari <90670993+kumarismri@users.noreply.github.com>
2026-05-14 15:38:00 +0530

    forgotwhichcommit2

--- replaceAndCapetelize.js
+++ replaceAndCapetelize.js
@@ -1,3 +1,6 @@
+// given a string contain "_" remove the "_" and 
+// convert first character of every word to uppercase
+
 function replace(str){
   let arr = str.split("");
   for(let i=0;i<arr.length;i++){
